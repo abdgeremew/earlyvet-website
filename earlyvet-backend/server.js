bf8f511156c86d01d45d5c5405a6d84d2f93ad28
abdgeremew <abdiggebeyehu@gmail.com>
2025-03-22 15:45:29 +0300

    Added start script for backend

--- earlyvet-backend/server.js
+++ earlyvet-backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const Contact = require('./models/contact'); // Import Contact model
+const Contact = require('./models/Contact'); // Import Contact model
 require('dotenv').config(); // Load environment variables
 
 // Initialize the app
